7f15c503f08b70812f70cf68acbe761bbdbe7ca9
narawry <nathedaeng@gmail.com>
2024-03-05 12:54:44 +0800

    0503

--- DSCI-AS1-code.py
+++ DSCI-AS1-code.py
@@ -27,7 +27,7 @@ if first_page == 'Dashboards':
     if dash_no == 'First (Main) Dashboard':
     
         # Using st.radio to divide gender
-        gender_rad = st.radio('Select a Gender:', ['Male', 'Female'])
+        gender_rad = st.radio('Select a Gender (applied to all) :', ['Male', 'Female'])
         filteredbygender = df.loc[df['Gender'] == gender_rad]
 
         # 1st Visualization (Bar)
@@ -46,14 +46,14 @@ if first_page == 'Dashboards':
         filtslide = st.slider(label=f'Adjust the filter:', min_value=min_value, max_value=max_value)
            
         filtered_df = filteredbygender[(filteredbygender['Stress_Level'] >= filtslide) &
-                                           (filteredbygender['Depression_Score'] >= filtslide) &
-                                           (filteredbygender['Anxiety_Score'] >= filtslide) &
-                                           (filteredbygender['Financial_Stress'] >= filtslide)]
+                                       (filteredbygender['Depression_Score'] >= filtslide) &
+                                       (filteredbygender['Anxiety_Score'] >= filtslide) &
+                                       (filteredbygender['Financial_Stress'] >= filtslide)]
             
         bar_chart = filtered_df[bar_att1].value_counts()
 
-        fig = px.bar(x=bar_chart.index, y=bar_chart.values, color=bar_chart.index, labels={'x': bar_att1, 'y': filter_att}, template='plotly_dark')
-        fig.update_layout(title="Correlation Between Mental Health Factors", xaxis_title=bar_att1, yaxis_title=filter_att, height=600, width=800)
+        fig = px.bar(x=bar_chart.index, y=bar_chart.values, color=bar_chart.index, labels={'x': bar_att1, 'y': filter_att})
+        fig.update_layout(title="Correlation Between Mental Health Factors", xaxis_title=bar_att1, yaxis_title=filter_att, height=700, width=900)
         st.plotly_chart(fig)
 
         # 2nd Visualization (Bar)
@@ -63,7 +63,7 @@ if first_page == 'Dashboards':
         bar_chart = filteredbygender.groupby(['Course', bar_att2]).size().reset_index(name='Count')
 
         fig = px.bar(bar_chart, x='Course', y='Count', color=bar_att2, labels={'x': 'Course', 'y': 'Count'}, template='plotly_dark')
-        fig.update_layout(title=f"Count for each {bar_att2} in different courses", xaxis_title='Course', yaxis_title='Count', height=600, width=800)
+        fig.update_layout(title=f"Count for each {bar_att2} in different courses", xaxis_title='Course', yaxis_title='Count', height=700, width=900)
         st.plotly_chart(fig)
 
         # 3rd Visualization (Bar)
@@ -73,7 +73,7 @@ if first_page == 'Dashboards':
         bar_chart = filteredbygender.groupby(['Relationship_Status', bar_att3]).size().reset_index(name='Count')
 
         fig = px.bar(bar_chart, x='Relationship_Status', y='Count', color=bar_att3, labels={'x': 'Relationship Status', 'y': 'Count'}, template='plotly_dark')
-        fig.update_layout(title=f"Count for each {bar_att3} in relationship status", xaxis_title='Relationship Status', yaxis_title='Count', height=600, width=800)
+        fig.update_layout(title=f"Count for each {bar_att3} in relationship status", xaxis_title='Relationship Status', yaxis_title='Count', height=700, width=900)
         st.plotly_chart(fig)
 
         # 4th Visualization (Area)
@@ -88,8 +88,8 @@ if first_page == 'Dashboards':
 
         area_chart = filtered_df[area_att].value_counts()
 
-        fig = px.area(x=area_chart.index, y=area_chart.values, labels={'x': area_att, 'y': 'Count'}, template='plotly_dark')
-        fig.update_layout(title=f"Count for each {area_att} in {category} category", xaxis_title=area_att, yaxis_title='Count', height=600, width=800)
+        fig = px.area(x=area_chart.index, y=area_chart.values, labels={'x': area_att, 'y': 'Count'})
+        fig.update_layout(title=f"Count for each {area_att} in {category} category", xaxis_title=area_att, yaxis_title='Count', height=700, width=900)
         st.plotly_chart(fig)
 
         # 5th Visualization (Area)
@@ -103,8 +103,8 @@ if first_page == 'Dashboards':
         filtered_df = filteredbygender[filteredbygender['Social_Support'] == socsup_cat]
         area_chart = filtered_df[area_att].value_counts()
 
-        fig = px.area(x=area_chart.index, y=area_chart.values, labels={'x': area_att, 'y': 'Count'}, template='plotly_dark')
-        fig.update_layout(title=f"Count for each {area_att} with {socsup_cat} Support", xaxis_title=area_att, yaxis_title='Count', height=600, width=800)
+        fig = px.area(x=area_chart.index, y=area_chart.values, labels={'x': area_att, 'y': 'Count'})
+        fig.update_layout(title=f"Count for each {area_att} with {socsup_cat} Support", xaxis_title=area_att, yaxis_title='Count', height=700, width=900)
         st.plotly_chart(fig)
 
         # 6th Visualization (Pie)
@@ -130,7 +130,7 @@ if first_page == 'Dashboards':
         no_total = pie_no.value_counts().values[0] if not pie_no.empty else 0
 
         fig = px.pie(names=['Yes', 'No'], values=[yes_total, no_total],
-                title=f"{pie_att} vs {pie_cat}", labels={'names': pie_cat, 'values': 'Count'}, template='plotly_dark')
+                title=f"{pie_att} vs {pie_cat}", labels={'names': pie_cat, 'values': 'Count'})
         fig.update_layout(height=650, width=850)
         st.plotly_chart(fig)
 
@@ -152,7 +152,7 @@ if first_page == 'Dashboards':
         bar_chart = filteredbygender.groupby(['Age', bar_att]).size().reset_index(name='Count')
 
         fig = px.bar(bar_chart, x='Age', y='Count', color=bar_att, labels={'x': 'Age', 'y': 'Count'}, template='plotly_dark')
-        fig.update_layout(xaxis_title='Age', yaxis_title='Count', height=600, width=800)
+        fig.update_layout(xaxis_title='Age', yaxis_title='Count', height=700, width=900)
         st.plotly_chart(fig)
     
         # 2nd Visualization
@@ -161,7 +161,7 @@ if first_page == 'Dashboards':
             
         fig = px.histogram(filteredbygender, x=hist_att, template='plotly_dark')
         fig.update_layout(xaxis_title=hist_att, yaxis_title="Count")
-        fig.update_traces(marker=dict(line=dict(width=1, color='White')))
+        fig.update_traces(marker=dict(line=dict(width=3, color='White')))
         st.plotly_chart(fig)
 
         # 3rd Visualization
@@ -174,7 +174,7 @@ if first_page == 'Dashboards':
                               'Anxiety_Score', 'Financial_Stress', 'Semester_Credit_Load'], index=1)
 
         fig = px.scatter(filteredbygender, x=x_axis, y=y_axis, color='Gender', template='plotly_dark')
-        fig.update_layout(xaxis_title=x_axis, yaxis_title=y_axis, height=600, width=800)
+        fig.update_layout(xaxis_title=x_axis, yaxis_title=y_axis, height=700, width=900)
         st.plotly_chart(fig)
 
         # 4th Visualization
@@ -185,7 +185,7 @@ if first_page == 'Dashboards':
         line_chart = filteredbygender.groupby(['Course', line_att]).size().reset_index(name='Count')
         
         fig = px.line(line_chart, x='Course', y='Count', color=line_att, labels={'x': 'Course', 'y': 'Count'}, template='plotly_dark')
-        fig.update_layout(xaxis_title='Course', yaxis_title='Count', height=600, width=800)
+        fig.update_layout(xaxis_title='Course', yaxis_title='Count', height=700, width=900)
         st.plotly_chart(fig)
 
         # 5th Visualization
@@ -209,5 +209,5 @@ if first_page == 'Dashboards':
         pie_chart.columns = ['Labels', 'Values']
 
         fig = px.pie(pie_chart, values='Values', names='Labels', template='plotly_dark')
-        fig.update_layout(height=650, width=850)
+        fig.update_layout(height=700, width=900)
         st.plotly_chart(fig)
